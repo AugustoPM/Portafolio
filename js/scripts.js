00956b68f873ecd699394d3f748ed24c709bfc0d
Isania Moreno <augusto_pmorche@hotmail.com>
2017-10-19 18:21:18 -0400

    javscript

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,56 @@ $(function(){
 		$('#modal').modal('hide');
 	});
 })
+
+var form = document.getElementsByClassName('formulario')
+form[0].addEventListener('submit', function(evt){
+	var nombre = form[0].nombre;
+	var email = form[0].email;
+	var mensaje = form[0].mensaje;
+	if (nombre.value=='' || nombre.value == 'Ingrese Nombre'){
+		evt.preventDefault();
+		nombre.value='Ingrese Nombre';
+		nombre.style.color= 'red';
+		nombre.style.border='solid 1px red'
+		nombre.focus();		
+	}else if(email.value == '' || nombre.value == 'Ingrese Email'){
+		evt.preventDefault();
+		email.value='Ingrese Email';
+		email.style.color= 'red';
+		email.style.border='solid 1px red'
+		email.focus();	
+	}else if(mensaje.value == '' || nombre.value == 'Escriba su Mensaje'){
+		evt.preventDefault();
+		mensaje.value='Escriba su Mensaje';
+		mensaje.style.color= 'red';
+		mensaje.style.border='solid 1px red'
+		mensaje.focus();	
+	}
+});
+
+
+
+var mensaje = document.getElementById('mensaje');
+var caracteresMaximos = 140 ;
+mensaje.addEventListener('keypress', function(evt){
+	var caracter = evt.charCode || evt.keyCode ;
+	//permitir flechas horizntales
+	if (caracter == 37 || caracter == 39){
+		return true;
+	}
+	//permitir borrar con backspace y supr
+	if (caracter == 8 || caracter == 46){
+		return true;
+	}else if (this.value.length >= caracteresMaximos){
+		evt.preventDefault ();
+	}
+});
+mensaje.addEventListener ('keyup' , function(){
+var info = document.getElementsByClassName('infocaract');
+if (this.value.length >= caracteresMaximos){
+	info[0].innerHTML = "Maximo" + caracteresMaximos + " caracteres";
+}else {
+	info[0].innerHTML = "Puede escribir Hasta " + (caracteresMaximos - this.value.length) + " caracteres";
+}
+
+});
